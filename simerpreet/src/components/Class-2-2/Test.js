6000ffaba6e550317c3b291a9adb6721fdf3011d
NehaBatra09 <87415922+NehaBatra09@users.noreply.github.com>
2023-06-05 14:09:41 +0530

    commit changes

--- simerpreet/src/components/Class-2-2/Test.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-import React from "react";
-
-
--- simerpreet/src/components/Loader.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Loader=()=>{
-
-    return(<><h1>Loading Please Wait...</h1></>)
-}
-export default Loader
