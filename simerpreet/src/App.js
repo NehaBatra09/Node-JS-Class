68777b0938f233ae484a2a9f9f3939927b219b21
NehaBatra09 <87415922+NehaBatra09@users.noreply.github.com>
2023-07-11 12:40:11 +0530

    commit changes

--- simerpreet/src/App.js
+++ simerpreet/src/App.js
@@ -7,18 +7,9 @@ import { Comp2 } from "./components/Project-RealState/Comp2";
 import { Comp3 } from "./components/Project-RealState/Comp3";
 const App = () => {
     
-    let First = HighOderCom(Comp1)
-    let Second = HighOderCom(Comp2)
-    let Third=HighOderCom(Comp3)
 
     return (<>
-        <BrowserRouter>
-            <Second/>
-        </BrowserRouter>
-        
-        <div className={"scroll"} id="main" >
-            <img src="https://thumbs.dreamstime.com/z/beautiful-exterior-home-pictures-new-home-design-images-modern-best-house-design-images-best-house-images-images-latest-172194515.jpg" />
-        </div>
+       <Comp1/>
     </>)
 
 
--- simerpreet/src/components/Project-RealState/Comp1.js
+++ simerpreet/src/components/Project-RealState/Comp1.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { user } from "./utils";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAddressCard, faAirFreshener } from '@fortawesome/free-solid-svg-icons'
 export const Comp1 = () => {
-
     return (<>
-        <h1>Trading...</h1>
-        {/* <CustomStyle></CustomStyle> */}
+        <div>
+            <h3><FontAwesomeIcon icon={faAddressCard} color="blue" /> Font Awesome Address Icon</h3>
+            <h3><FontAwesomeIcon icon={faAirFreshener} color="red" /> Font Awesome Solid Icon</h3>
+        </div>
     </>)
 }
